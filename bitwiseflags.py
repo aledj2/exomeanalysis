2af80cfefe845e0aeb9957b8e9fd9905891831e6
Aled Jones <Aledjones@nhs.net>
2015-03-14 15:24:39 +0000

    prints bit flag output

--- bitwiseflags.py
+++ bitwiseflags.py
@@ -3,3 +3,15 @@ Created on 14 Mar 2015
 
 @author: aled
 '''
+import sys
+
+bitflag={1:"template having multiple segments in sequencing", 2:"each segment properly aligned according to the aligner", 4:"segment unmapped",8:"next segment in the template unmapped", 10:"SEQ being reverse complemented"}
+
+
+def printflag(bitno):
+    print "bitflag="+str(bitno)+", "+'{0:012b}'.format(bitno)
+    print bitflag [bitno]
+
+printflag(2)
+#printflag(sys.argv[1])
+
