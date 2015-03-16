6abb200c048d5a4eb5aab8f00c7efe1470dbd4bb
aledj2 <aledjones@nhs.net>
2015-03-16 11:13:38 +0000

    removed empty lines

--- bitwiseflags.py
+++ bitwiseflags.py
@@ -8,10 +8,12 @@ import sys
 bitflag={11:"template having multiple segments in sequencing", 10:"each segment properly aligned according to the aligner", 9:"segment unmapped",8:"next segment in the template unmapped", 7:"SEQ being reverse complemented", 6:"SEQ of the next segment in the template being reversed", 5:"the first segment in the template", 4:"the last segment in the template", 3:"secondary alignment", 2:"not passing quality control",1:"PCR or optical duplicate",0:"supplementary alignment"}
 #print bitflag[11]
 
+
 def convert_to_bin(number):
     binary='{0:012b}'.format(int(number))
     #print binary
     printflag(number,binary)
+
 
 def printflag(bitno,binary):
     print "bitflag="+str(bitno)+", "+binary
@@ -20,10 +22,8 @@ def printflag(bitno,binary):
         if binary[i] == '1':
             #print i
             print bitflag[i]
-             
-        
-        
-        
+
+
 #convert_to_bin(147)
 convert_to_bin(sys.argv[1])
 
