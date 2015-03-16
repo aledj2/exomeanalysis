438d1ebcf18d310038e64134a48c0a623c955f33
aledj2 <aledjones@nhs.net>
2015-03-16 11:11:25 +0000

    Corrected to print out multiple bit flags

--- bitwiseflags.py
+++ bitwiseflags.py
@@ -5,13 +5,25 @@ Created on 14 Mar 2015
 '''
 import sys
 
-bitflag={1:"template having multiple segments in sequencing", 2:"each segment properly aligned according to the aligner", 4:"segment unmapped",8:"next segment in the template unmapped", 10:"SEQ being reverse complemented", 20:"SEQ of the next segment in the template being reversed", 40:"the first segment in the template", 80:"the last segment in the template", 100:"secondary alignment", 200:"not passing quality control",400:"PCR or optical duplicate",800:"supplementary alignment"}
+bitflag={11:"template having multiple segments in sequencing", 10:"each segment properly aligned according to the aligner", 9:"segment unmapped",8:"next segment in the template unmapped", 7:"SEQ being reverse complemented", 6:"SEQ of the next segment in the template being reversed", 5:"the first segment in the template", 4:"the last segment in the template", 3:"secondary alignment", 2:"not passing quality control",1:"PCR or optical duplicate",0:"supplementary alignment"}
+#print bitflag[11]
 
+def convert_to_bin(number):
+    binary='{0:012b}'.format(int(number))
+    #print binary
+    printflag(number,binary)
 
-def printflag(bitno):
-    print "bitflag="+str(bitno)+", "+'{0:012b}'.format(int(bitno))
-    print bitflag [int(bitno)]
-
-#printflag(2)
-printflag(sys.argv[1])
+def printflag(bitno,binary):
+    print "bitflag="+str(bitno)+", "+binary
+    #print bitflag [int(bitno)]
+    for i in xrange (11,0,-1):
+        if binary[i] == '1':
+            #print i
+            print bitflag[i]
+             
+        
+        
+        
+#convert_to_bin(147)
+convert_to_bin(sys.argv[1])
 
